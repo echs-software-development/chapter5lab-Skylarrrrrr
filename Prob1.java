afce5bb892e7a38df45a815132d81b8ee2de6e1f
SKYLAR DEITZ <sd1081@students.necsd.net>
2021-03-04 20:52:28 +0000

    Problem 1

--- Prob1.java
+++ Prob1.java
@@ -1,7 +1,29 @@
-import java.util.Scanner;
-
+import java.util.Scanner;  
 public class Prob1 {
-  public static void main(String[] args){
 
-  }
-}
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		System.out.print("How many grades would you like to enter: ");
+		
+		int times = scan.nextInt();
+		int sumOfGrades = 0; //outside the loop so condition does not get set to 0
+		//every time the loop iterates
+		
+		for (int i = 1; i <= times;i++) {
+			
+			System.out.println("Enter grade " + i + "-->");
+			int grade = scan.nextInt();//inside the loop- eval to this grade only
+			//sumOfGrades = sumOfGrades + grade;
+			sumOfGrades += grade;
+			
+		}
+		
+		System.out.println("The sum of the grades is:: " + sumOfGrades);
+			
+			
+		}
+		
+		
+
+	}
--- Prob2.java
+++ Prob2.java
@@ -3,5 +3,33 @@ import java.util.Scanner;
 public class Prob2 {
   public static void main(String[] args){
 
+    int countP = 0;
+int countN = 0;
+    int count = 0, total = 0, number;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter an integer, the code ends if it is a 0:");
+    number = input.nextInt();
+
+    while (number != 0) {
+      if (number > 0)
+        countP++;
+      else if (number < 0)
+        countN++;
+      
+      total += number;
+      count++;
+
+      number = input.nextInt();
+    }
+
+    if (count == 0)
+      System.out.println("No numbers are entered except 0.");
+    else {
+      System.out.println("The number of positives is: " + countP);
+      System.out.println("The number of negatives is: " + countN);
+      System.out.println("The total is: " + total);
+      System.out.println("The average is: " + total * 1.0 / count);
+    }
   }
 }
